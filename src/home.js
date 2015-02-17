3fe2806f72e204dd26b18266841ede57e2c2497e
Shtian <hagastian@gmail.com>
2015-02-17 09:45:02 +0100

    sort array by date added

--- src/home.js
+++ src/home.js
@@ -9,6 +9,7 @@ export class Home{
     var that = this;
     this.torrentsRef.on('value', function(data){
       that.torrents = data.val();
+      that.torrents = _.sortBy(that.torrents, 'addedDate').reverse();
       // Normalize values
       that.torrents = _.map(that.torrents, function(item){
         item.totalSize = Math.floor(item.totalSize / 1000000);
